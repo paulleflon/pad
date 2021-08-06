ca35b39a5fbe4fc71baf9b0059e41cc66fe2a9f3
Hicka <hello@hicka.dev>
2021-08-06 14:37:25 +0200

    feat(pad): add `buttonProperties` to PadState

--- src/types/PadState.ts
+++ src/types/PadState.ts
@@ -1,7 +1,13 @@
+import ButtonProperties from './ButtonProperties';
+
 /**
  * The state of a Pad component.
  */
 interface PadState {
+	/**
+	 * Array of the Pad Buttons' properties. 2 dimensional array mapped as the [y][x] positions of the buttons. 
+	 */
+	buttonProperties: ButtonProperties[][];
 	/**
 	 * Array of pressed keys' codes.
 	 */
