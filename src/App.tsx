201aac8d75651a200087cbc0b295bbce926e78fb
Hicka <hello@hicka.dev>
2021-08-11 15:14:33 +0200

    feat(components): add TitleBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Pad from './components/Pad';
-
+import TitleBar from './components/TitleBar';
 function App(): React.ReactElement {
 	return (
 		<div className="App">
+			<TitleBar />
 			<Pad />
 		</div>
 	);
---
+++ src/components/TitleBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../style/TitleBar.sass';
+
+function TitleBar(): React.ReactElement {
+	return (
+		<div className='title-bar'>
+			<div className='app-name'>Pad</div>
+			<div className='action-buttons'>
+				<div className='action-button' onClick={window.electron.minimize}>
+					<span className='material-icons'>horizontal_rule</span>
+				</div>
+				<div className='action-button' onClick={window.electron.toggleMaximized}>
+					<span className='material-icons' id='maximize'>crop_square</span>
+				</div>
+				<div className='action-button' id='close' onClick={window.electron.closeApp}>
+					<span className='material-icons'>close</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default TitleBar;
