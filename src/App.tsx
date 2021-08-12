6eb9478293739ccaf37cd54f7d8aba9ac663243b
Hicka <hello@hicka.dev>
2021-08-12 12:28:25 +0200

    fix(design): fix application layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Pad from './components/Pad';
 import TitleBar from './components/TitleBar';
 function App(): React.ReactElement {
 	return (
-		<div className="App">
+		<>
 			<TitleBar />
-			<Pad />
-		</div>
+			<div className="App">
+				<Pad />
+			</div>
+		</>
 	);
 }
 
